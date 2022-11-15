5e842c19063d900df7d4ecf77a768f624b73b771
danielaguileta10 <daniel.aguileta@unah.hn>
2022-11-15 14:06:41 -0600

    Subida proyecto

---
+++ resources/views/dash/index.blade.php
@@ -0,0 +1,58 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <center><h1 >BIENVENIDA AL SISTEMA DE COMIDAS NOYSITA</h1></center>
+@stop
+
+@section('content')
+    
+<div class="py-5 service-22">
+    <div class="container">
+        <!-- Row -->
+        <div class="row wrap-service-22">
+            <!-- Column -->
+            <div class="col-lg-6"> 
+							<img src="https://scontent.ftgu3-4.fna.fbcdn.net/v/t1.6435-9/129140154_3305335326244059_3905055941769180687_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFC4YIN9mcLamh1oQBEAEGgf97TwleLy6l_3tPCV4vLqUdWAyUJpV0fCFqofQ9ZOEpP-Wj0CtYxNnp9vN2QplqY&_nc_ohc=DmcnliB_LZIAX-Yz3Fg&_nc_ht=scontent.ftgu3-4.fna&oh=00_AfDE62_fpzp9uIW5kOG8VELPt1lfPHUXmJ7fjyn2R0QvhQ&oe=6381826A" class="rounded img-shadow img-fluid" alt="wrapkit" /> 
+						</div>
+            <!-- Column -->
+            <div class="col-lg-6 mt-4 mt-md-0">
+                <div class="text-box">
+                    <h1>NUESTRA COMIDA REPRESENTA:</h1>
+                    <h2><p>los sabores de nuestra amada tierra Hondura sansonada con mucho amor para que su sabor sea el mejor </p></h2>
+                    <h2><p></p></h2>
+								
+								</div>
+            </div>
+            
+            <!-- Column -->
+            <div class="row wrap-service-22">
+                <!-- Column -->
+                <div class="col-lg-6"> 
+                    <p></p>
+                                <img src="https://scontent.ftgu3-4.fna.fbcdn.net/v/t39.30808-6/277225903_4719219094855668_2448278454657120355_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeH0QIC2OO0qLhYvgV3akkQLLXmvQB9mfHotea9AH2Z8esgiTgUNGC8MTM-1m0XXVjsYWPQAKD-5szousFW7mvi4&_nc_ohc=IddS-IH-ugIAX9yvjFH&_nc_ht=scontent.ftgu3-4.fna&oh=00_AfBtKAZG7HtOuWhs5GOe4PH2DRWR26d6Go-q2q5ncXqlBw&oe=636146EC" class="rounded img-shadow img-fluid" alt="wrapkit" /> 
+                            </div>
+                <!-- Column -->
+                <div class="col-lg-6 mt-4 mt-md-0">
+                    <div class="text-box">
+                        <h1>COMIDAS NOSYSITA  DICE: </h1>
+                       <h2><p>Siempre un placer atenderles en nuestras instalaciones de Tela, Atlantida Como equipo de trabajo hacemos lo mejor posible para que su visita sea agradable y que disfrute de nuestra rico sazón casero en un ambiente seguro, familiar.</p></h2>
+
+                                            <a class="btn btn-info-gradiant btn-md text-white border-0" href="#f20"><span>View Details</span></a>
+                                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
