c2e55ef2b358d569829b4aacd46059f1091e5570
danielaguileta10 <daniel.aguileta@unah.hn>
2022-11-15 14:29:58 -0600

    actualizacion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+       
         Validator::extend('alpha_spaces', function ($attribute, $value) {
 
             // This will only accept alpha and spaces. 
             // If you want to accept hyphens use: /^[\pL\s-]+$/u.
             return preg_match('/^[\pL\s]+$/u', $value); 
+            
     
         });
     }
